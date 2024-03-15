6f7fbf3cdd00da8be4523a6033360fed528e8384
Rian <rianatgithub@gmail.com>
2024-03-15 18:57:09 +0700

    feat(Praktikum 2.4): Retreiving or Creating Models

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,12 +6,12 @@
             <div class="col-12">
                 <h1>Data User</h1>
                 <table border="1" cellpadding="2" cellspacing="0">
-                    {{-- <tr>
+                    <tr>
                         <th>ID</th>
                         <th>Username</th>
                         <th>Nama</th>
                         <th>ID Level Pengguna</th>
-                    </tr> --}}
+                    </tr>
                     {{-- @foreach($data as $d)
                     <tr>
                         <td>{{ $d->user_id }}</td>
@@ -20,18 +20,18 @@
                         <td>{{ $d->level_id }}</td>
                     </tr>
                     @endforeach --}}
-                    {{-- <tr>
+                    <tr>
                         <td>{{ $data->user_id }}</td>
                         <td>{{ $data->username }}</td>
                         <td>{{ $data->nama }}</td>
                         <td>{{ $data->level_id }}</td>
-                    </tr> --}}
-                    <tr>
+                    </tr>
+                    {{-- <tr>
                         <td>Jumlah Pengguna</td>
                     </tr>
                     <tr>
                         <td>{{ $data }}</td>
-                    </tr>
+                    </tr> --}}
                 </table>
             </div>
         </div>
