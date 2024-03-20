80b229bf32bdafcb5b3d3984f2ff2a3900d73d9c
Rian <rianatgithub@gmail.com>
2024-03-20 12:00:46 +0700

    feat(Praktikum 2): Integrasi dengan DataTables

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\DataTables\KategoriDataTable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class KategoriController extends Controller
 {
-    public function index() {
+    public function index(KategoriDataTable $dataTable)
+    {
         // $data = [
         //     'kategori_kode' => 'SNK',
         //     'kategori_nama' => 'Snack/Makanan Ringan',
@@ -22,7 +24,8 @@ class KategoriController extends Controller
         // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
         // return "Delete data berhasil. Jumlah data yang dihapus $row baris";
 
-        $row = DB::table('m_kategori')->get();
-        return view('kategori', ['data' => $row]);
+        // $row = DB::table('m_kategori')->get();
+        // return view('kategori', ['data' => $row]);
+        return $dataTable->render('kategori.index');
     }
 }
