7c23ca2b16bd3c286ee6aedafbeae826d10c8892
Rian <rianatgithub@gmail.com>
2024-03-27 19:47:10 +0700

    feat(D): CRUD(create, read, update, delete)

--- routes/web/web.php
+++ routes/web/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\POSController;
 use App\Http\Controllers\user\UserController;
 
 /*
@@ -49,3 +50,7 @@ Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::post('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+
+
+Route::resource('m_user', POSController::class);
