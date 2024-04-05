15c4d685cc0facf6ded64d75a22cf2b3bf294408
Rian <rianatgithub@gmail.com>
2024-04-05 15:18:38 +0700

    feat(Praktikum 3): Implementasi jQuery Datatable di AdminLTE

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,8 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-use App\Models\LevelModel;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UserModel extends Model
@@ -14,11 +12,11 @@ class UserModel extends Model
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-
+    public $timestamps = false;
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
     public function level(): BelongsTo
     {
-        return $this->belongsTo(LevelModel::class);
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
 }
--- routes/web/web.php
+++ routes/web/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\POSController;
 use App\Http\Controllers\user\UserController;
 
 /*
@@ -23,34 +20,47 @@ use App\Http\Controllers\WelcomeController;
 // require __DIR__ . '/product/product.php';
 // require __DIR__ . '/user/user.php';
 // require __DIR__ . '/transaction/transaction.php';
-require __DIR__ . '/forms/forms.php';
-require __DIR__ . '/user/level.php';
+// require __DIR__ . '/forms/forms.php';
+// require __DIR__ . '/user/level.php';
 
 Route::get('/', [WelcomeController::class, 'index']);
 
-// Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
-Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
-Route::post('/kategori/store', [KategoriController::class, 'store'])->name('kategori.store');
-Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
-Route::post('/kategori/update/{id}', [KategoriController::class, 'update'])->name('kategori.update');
-Route::get('/kategori/delete/{id}', [KategoriController::class, 'delete'])->name('kategori.delete');
-
-
-Route::get('/user', [UserController::class, 'index']);
-
-// Route::get('/', function () {
-//     return view('home');
-// });
-
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-Route::post('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+Route::group(['prefix' => 'user'], function() {
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('/create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
 
 
 
-Route::resource('m_user', POSController::class);
+// // Route::get('/level', [LevelController::class, 'index']);
+// Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
+// Route::get('/kategori', [KategoriController::class, 'index'])->name('kategori.index');
+// Route::post('/kategori/store', [KategoriController::class, 'store'])->name('kategori.store');
+// Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
+// Route::post('/kategori/update/{id}', [KategoriController::class, 'update'])->name('kategori.update');
+// Route::get('/kategori/delete/{id}', [KategoriController::class, 'delete'])->name('kategori.delete');
+
+
+// Route::get('/user', [UserController::class, 'index']);
+
+// // Route::get('/', function () {
+// //     return view('home');
+// // });
+
+// Route::get('/user/tambah', [UserController::class, 'tambah']);
+// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
+// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+// Route::post('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+
+// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+
+
+// Route::resource('m_user', POSController::class);
