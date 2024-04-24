05ed9bd5f36c6eb100e1abed270567e608af80fe
Rian <rianatgithub@gmail.com>
2024-04-24 13:05:31 +0700

    feat(Praktikum): Auth dan Middleware

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserModel extends Model
+use Illuminate\Foundation\Auth\User as Authenticable;
+
+class UserModel extends Authenticable
 {
     use HasFactory;
 
--- routes/web/web.php
+++ routes/web/web.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\kategori\KategoriController;
 use App\Http\Controllers\barang\BarangController;
 use App\Http\Controllers\stok\StokController;
 use App\Http\Controllers\penjualan\PenjualanController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ManagerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -94,4 +97,25 @@ Route::group(['prefix' => 'penjualan'], function() {
     Route::get('/{id}/edit', [PenjualanController::class, 'edit']);
     Route::put('/{id}', [PenjualanController::class, 'update']);
     Route::delete('/{id}', [PenjualanController::class, 'destroy']);
+});
+
+
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::get('register', [AuthController::class, 'register'])->name('register');
+Route::post('proses_login', [AuthController::class, 'proses_login'])->name('proses_login');
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('proses_register', [AuthController::class, 'proses_register'])->name('proses_register');
+
+// kita atur juga untuk middleware menggunakan group pada routing
+// di dalamnya terdapat group untuk mengecek kondisi login
+// jika user yang login merupakan admin maka akan diarahkan ke AdminController
+// jika user yang login merupakan manager makan akan diarahkan ke UserController
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::group(['middleware' => ['cek_login:1']], function () {
+        Route::resource('admin', AdminController::class);
+    });
+    Route::group(['middleware' => ['cek_login:2']], function () {
+        Route::resource('manager', ManagerController::class);
+    });
 });
