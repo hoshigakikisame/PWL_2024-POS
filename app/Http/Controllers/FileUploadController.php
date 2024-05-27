9681b5b09bd369512189753e4f1f1ebf9a0c08aa
Rian <rianatgithub@gmail.com>
2024-05-27 11:22:17 +0700

    feat(D): MEMINDAH FILE UPLOAD

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -16,6 +16,9 @@ class FileUploadController extends Controller
         $request->validate([
             'berkas' => 'required|file|image|max:500'
         ]);
-        echo $request->berkas->getClientOriginalName()." lolos validasi";
+        $extFile = $request->berkas->getClientOriginalName();
+        $namaFile = 'web-'.time().".".$extFile;
+        $path = $request->berkas->storeAs('uploads', $namaFile);
+        echo "proses upload berhasil, data disimpan pada: $path";
     }
 }
