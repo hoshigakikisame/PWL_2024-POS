fa670d23e30a0745138fd89fff55cce2a1fff8a3
Rian <rianatgithub@gmail.com>
2024-05-29 13:07:26 +0700

    feat: Tugas

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -18,7 +18,35 @@ class FileUploadController extends Controller
         ]);
         $extFile = $request->berkas->getClientOriginalName();
         $namaFile = 'web-'.time().".".$extFile;
-        $path = $request->berkas->storeAs('uploads', $namaFile);
-        echo "proses upload berhasil, data disimpan pada: $path";
+
+        $path = $request->berkas->move('gambar', $namaFile);
+        $path = str_replace("\\", "//", $path);
+        echo "Variable path berisi: $path <br>";
+
+        $pathBaru = asset('gambar/'.$namaFile);
+        echo "Proses upload berhasil, data disimpan pada: $path";
+        echo "<br>";
+        echo "Tampilkan link: <a href='$pathBaru'>$pathBaru</a>";
+    }
+
+    public function fileUploadRename() {
+        return view('file-upload-rename');
+    }
+
+    public function prosesFileUploadRename() {
+        request()->validate([
+            'filename' => 'required|max:255',
+            'file' => 'required|file|image|max:500'
+        ]);
+
+        $file = request()->file('file');
+
+        $ext = $file->getClientOriginalExtension();
+        $newName = request()->filename . "." . $ext;
+        $path = $file->storeAs('uploads', $newName);
+
+        echo "Gambar berhasil di upload ke <a href='storage/$path'>$newName</a>";
+        echo "<br><br>";
+        echo "<img width=500 src='storage/$path'/>";
     }
 }
